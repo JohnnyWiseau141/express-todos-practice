0b324ea1cc0fbabe2c7febc682886edc71764ea6
JohnnyWiseau141 <john.a.foley141@gmail.com>
2023-03-09 13:28:16 -0500

    get that banana outta here

--- routes/users.js
+++ routes/users.js
@@ -6,11 +6,6 @@ router.get('/', function(req, res) {
   res.send('respond with a resource')
 })
 
-router.get("/banana", function(req, res) {
-  console.log("the /banana route lol")
-  res.send('<h1>Testing ONE TWO TESTING!!!!<h2>')
-})
-
 export {
   router
 }
